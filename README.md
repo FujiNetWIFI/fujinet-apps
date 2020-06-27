6f4491a99f29a1aeb1c6b670bf0ce146eb41c37a
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2020-06-27 10:55:39 -0500

    Add apps.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # fujinet-apps
 Applications for FujiNet
+
+* PLATOTERM - a Version of the PLATO terminal for IRATA.ONLINE, pre-configured to connect to IRATA.ONLINE
+* boot-disk-cc65-template - An example project that creates a single density (90K boot disk) that runs a program.
---
+++ boot-disk-cc65-template/src/main.c
@@ -0,0 +1,19 @@
+/**
+ * main for cc65 boot disk template
+ *
+ * Author:
+ *  Thomas Cherryhomes <thom.cherryhomes@gmail.com>
+ *
+ * Licensed under GPL 3.0, read LICENSE in top
+ * level folder for details.
+ */
+
+#include <atari.h>
+
+void main(void)
+{
+  while (1)
+    {
+      OS.colr2++;
+    }
+}
