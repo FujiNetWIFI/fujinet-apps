a4a9eaca3bea047562037664808323daf40219a6
Bill Kendrick <nbs@sonic.net>
2021-04-14 00:58:00 -0700

    Get current date; allow chosing date
    Simple date-picker functionality in the menu/title.
    Note: Does not actually alter requests (so still always
    fetching "today's" APOD).  Need to add to request, and server
    needs to grok and deal with it.
    
    So, a WIP.

--- apod/apod.c
+++ apod/apod.c
@@ -14,10 +14,11 @@
 #include <atari.h>
 #include <peekpoke.h>
 #include <string.h>
+#include "sio.h"
 #include "nsio.h"
 #include "dli.h"
 
-#define VERSION "2021-04-13"
+#define VERSION "VER. 2021-04-13"
 
 /* In ColorView mode, we will have 3 display lists that
    we cycle through, each interleaving between three
@@ -29,6 +30,10 @@ extern unsigned char rgb_table[];
 /* A block of space to store the graphics */
 extern unsigned char scr_mem[];
 
+/* Storage for the current date/time */
+unsigned char time_buf[6];
+unsigned char cur_yr, cur_mo, cur_day, pick_yr, pick_mo, pick_day;
+
 /* Values for Red, Green, and Blue, to allow hue adjustment */
 unsigned char rgb_red, rgb_grn, rgb_blu;
 
@@ -38,6 +43,8 @@ unsigned char rgb_red, rgb_grn, rgb_blu;
 #define DEFAULT_RGB_RED 0x20 /* 2 "orange" */
 #define DEFAULT_RGB_GRN 0xC0 /* 12 "green" */
 #define DEFAULT_RGB_BLU 0xB0 /* 11 "blue green" */
+
+unsigned char last_day[13] = { 0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
 /**
  * Simple text rendering onto screen memory
@@ -416,6 +423,55 @@ void handle_rgb_keypress(unsigned char k) {
   }
 }
 
+/**
+ * Get time from #FujiNet via APETIME protocol
+ * (see https://github.com/FujiNetWIFI/fujinet-platformio/wiki/Accessing-the-Real-Time-Clock)
+ */
+void get_time() {
+  memset(time_buf, 0, 6);
+  OS.dcb.ddevic = 0x45; /* APETIME protocol */
+  OS.dcb.dunit = 0x01; /* unit 1 */
+  OS.dcb.dcomnd = 0x93; /* GETTIME request */
+  OS.dcb.dstats = 0x40; /* receive */
+  OS.dcb.dbuf = (void *) time_buf;
+  OS.dcb.dtimlo = 15; /* timeout (seconds) */
+  OS.dcb.dunuse = 0;
+  OS.dcb.dbyt = (unsigned int) 6; /* reading 6 characters */
+  OS.dcb.daux1 = 0xee;
+  OS.dcb.daux2 = 0xa0;
+  siov();
+  if (time_buf[0] != 0) {
+    cur_yr = time_buf[2];
+    cur_mo = time_buf[1];
+    cur_day = time_buf[0];
+  } else {
+    /* This shouldn't happen */
+    cur_yr = 99;
+    cur_mo = 12;
+    cur_day = 31;
+  }
+  pick_yr = 0;
+  pick_mo = 0;
+  pick_day = 0;
+}
+
+void show_chosen_date() {
+  char str[20];
+
+  if (pick_day != 0) {
+    sprintf(str, "20%02d-%02d-%02d", pick_yr, pick_mo, pick_day);
+    myprint(2, 17, str);
+  } else {
+    myprint(2, 17, "current   ");
+  }
+}
+
+void pick_today() {
+  pick_yr = cur_yr;
+  pick_mo = cur_mo;
+  pick_day = cur_day;
+}
+
 /* The program! */
 void main(void) {
   unsigned char keypress, choice;
@@ -434,6 +490,9 @@ void main(void) {
   rgb_red = DEFAULT_RGB_RED;
   rgb_grn = DEFAULT_RGB_GRN;
   rgb_blu = DEFAULT_RGB_BLU;
+
+  /* Get the current date/time, if we can */
+  get_time();
 
   do {
     /* Prompt user for the preferred viewing mode */
@@ -460,8 +519,10 @@ void main(void) {
 
                   /*--------------------*/
     myprint(0, 15, "[0] get apod");
-    myprint(0, 16, "[1-4] get samples");
-    myprint(0, 17, "[5] color bars");
+    myprint(0, 16, "[<=>] change date");
+    show_chosen_date();
+    myprint(0, 18, "[1-4] get samples");
+    myprint(0, 19, "[5] color bars");
   
     baseurl = default_baseurl;
   
@@ -491,6 +552,48 @@ void main(void) {
             myprint(19, 23, " ");
           }
         }
+      }
+
+      if (keypress == KEY_LESSTHAN) {
+        if (pick_day == 0) {
+          pick_today();
+        }
+        if (pick_day > 1) {
+          pick_day--;
+        } else {
+          if (pick_mo > 1) {
+            pick_mo--;
+          } else {
+            pick_yr--;
+            pick_mo = 12;
+          }
+          pick_day = last_day[pick_mo];
+        }
+        show_chosen_date();
+      } else if (keypress == KEY_GREATERTHAN) {
+        if (pick_day == 0) {
+          pick_today();
+        }
+        if (pick_day < last_day[pick_mo]) {
+          pick_day++;
+        } else {
+          pick_day = 1;
+          if (pick_mo < 12) {
+            pick_mo++;
+          } else {
+            pick_mo = 1;
+            pick_yr++;
+          }
+        }
+        if (pick_yr > cur_yr ||
+            (pick_yr == cur_yr && pick_mo > cur_mo) ||
+            (pick_yr == cur_yr && pick_mo == cur_mo && pick_day > cur_day)) {
+          pick_today();
+        }
+        show_chosen_date();
+      } else if (keypress == KEY_EQUALS) {
+        pick_day = 0;
+        show_chosen_date();
       }
     } while (choice == CHOICE_NONE);
 
