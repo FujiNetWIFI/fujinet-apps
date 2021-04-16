110628b76c43889fa29eeb234b810679f287287d
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2021-04-15 19:48:04 -0500

    argh

--- vt100/src/term.c
+++ vt100/src/term.c
@@ -1,25 +1,27 @@
 /**
- * ADM-3A Terminal State Machine
+ * VT100 Terminal State Machine
  */
 
 #include <atari.h>
+#include <string.h>
+#include <stdlib.h>
 #include "conio.h"
 #include "term.h"
 
 enum {
       CHAR,
       ESCAPE,
-      SET_MODE,
-      SET_ROW,
-      SET_COL
+      COMMAND
 } mode=CHAR;
 
 bool term(char* buf, unsigned short len)
 {
-  char c;
-  char ca[8]={0,0,0,0,0,0,0,0};
-  unsigned short oldcol;
-  unsigned char oldrow;
+  char c;                // Character to output to screen (0 = nothing)
+  unsigned char p[16];   // 16 possible parameter values per esc sequence
+  unsigned char* pp;     // Pointer to next parameter to set.
+  char t[8];             // temp char array for parameter values
+  char* tp;              // T pointer
+  char or, oc;           // saved col/row
   
   do
     {
@@ -35,73 +37,121 @@ bool term(char* buf, unsigned short len)
 	      c=0x7E;  // ATASCII BS
 	      break;
 	    case 0x0A: // LF
+	    case 0x0B: // VT
+	    case 0x0C: // FF
 	      c=0;
-	      oldcol=OS.colcrs;
-	      ca[0]=0x9B; // EOL
-	      print(ca);
-	      OS.colcrs=oldcol;
-	      break;
-	    case 0x0B: // VT (upline)
-	      if (OS.colcrs>0)
-		c=0x1C; // UP ARROW
-	      else
-		c=0x00;
-	      break;
-	    case 0x0C: // FF ->
-	      c=0x1F;
+	      if (OS.rowcrs<24)
+		{
+		  print("\x1D"); // Cursor down
+		}
+	      else // Don't move cursor, but scoot screen upward.
+		{
+		  OS.rowcrs=0;
+		  print("\x9C"); // Delete line
+		  OS.rowcrs=23;
+		}
 	      break;
 	    case 0x0D: // CR
 	      c=0;
 	      OS.colcrs=0;
 	      break;
-	    case 0x1A: // Clear screen
-	      c=0x7D;  // ATASCII CLS
-	      break;
 	    case 0x1B: // ESCAPE
 	      mode=ESCAPE;
 	      c=0;
-	      break;
-	    case 0x1E: // Home cursor
-	      c=0;
-	      OS.rowcrs=OS.colcrs=0;
 	      break;
 	    }
 	  break;
 	case ESCAPE:
 	  switch(c=*buf++)
 	    {
-	    case '=': // Absolute position
+	    case '[':
+	      mode=COMMAND;
+	      tp=&t[0]; // Beginning of tmp parameter
+	      pp=&p[0]; // Beginning of parameters
+	      memset(t,0,sizeof(t)); // clear temp
 	      c=0;
-	      mode=SET_COL;
 	      break;
-	    case 'G': // Set mode
-	      c=0;
-	      mode=SET_MODE;
-	      break;
-	    default:
-	      c=0;
-	      mode=CHAR; // Go back to character mode.
 	    }
 	  break;
-	case SET_MODE:
-	  break;
-	case SET_ROW:
+	case COMMAND:
 	  c=*buf++;
-	  if ((c>0x19) && (c<0x38))
+	  if ((c>'/') && (c<':'))
 	    {
-	      OS.rowcrs=c-0x20;
+	      // Numeric value, concatenate to tmp
+	      *tp=c;
+	      tp++;
 	      c=0;
 	    }
-	  mode=CHAR;
-	  break;
-	case SET_COL:
-	  c=*buf++;
-	  if ((c>0x19) && (c<0x70))
+	  else if (c==';') // Next parameter
 	    {
-	      OS.colcrs=c-0x20;
+	      *pp=atoi(t);           // convert from tmp to param value
+	      pp++;                  // increment parameter value
+	      memset(t,0,sizeof(t)); // Clear tmp
 	      c=0;
 	    }
-	  mode=SET_ROW;
+	  else // Command
+	    {
+	      *pp=atoi(t);           // convert from tmp to param value
+	      memset(t,0,sizeof(t)); // clear tmp
+
+	      switch(c)              // c contains letter command
+		{
+		case 'A':            // Cursor UP (stop at top)
+		  if (OS.rowcrs>0)
+		    c=0x1C;          // ATASCII UP
+		  else
+		    c=0x00;
+		  break;
+		case 'B':            // Cursor DOWN (stop at bottom)
+		  if (OS.rowcrs<23)
+		    c=0x1D;
+		  else
+		    c=0x00;
+		  break;
+		case 'C':            // Cursor RIGHT (stop at right)
+		  if (OS.colcrs<79)
+		    c=0x1F;
+		  else
+		    c=0x00;
+		  break;
+		case 'D':            // Cursor LEFT (stop at left)
+		  if (OS.colcrs>0)
+		    c=0x1E;
+		  else
+		    c=0x00;
+		  break;
+		case 'H':            // Cursor position
+		  if ((p[0]==0) && (p[1]==0))
+		    OS.rowcrs=OS.colcrs=0;
+		  else
+		    {
+		      OS.rowcrs=p[0]-1;
+		      OS.colcrs=p[1]-1;
+		    }
+		  break;
+		case 'J':            // Clear
+		  c=0;               // consume char
+		  switch(p[0])       // 1st parameter gives clear type
+		    {
+		    case 0:          // clear from current pos to bottom 
+		      break;
+		    case 1:          // clear from top to current pos
+		      break;
+		    case 2:          // CLEAR WHOLE SCREEN
+		      or=OS.rowcrs;
+		      oc=OS.colcrs;
+		      print("\x7D"); // ATASCII CLS char
+		      OS.rowcrs=or;
+		      OS.colcrs=oc;
+		      break;
+		    }
+		  break;
+		case 'm':            // Mode
+		  break;
+		}
+	      
+	      mode=CHAR;             // Back to char mode.
+	    }
 	  break;
 	}
 
