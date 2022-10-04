291ab06e653f4900c6f2fb7e3e51b25710e97834
Thomas <thom.cherryhomes@gmail.com>
2022-10-04 00:03:06 -0500

    add topics scaffolding.

--- news/apple2/src/topics.c
+++ news/apple2/src/topics.c
@@ -14,15 +14,33 @@
 
 static TopicsState subState=TOPICS_SHOW;
 
+TopicsState topics_show(void)
+{
+  return TOPICS_SHOW;
+}
+
+TopicsState topics_up(void)
+{
+  return TOPICS_SHOW;
+}
+
+TopicsState topics_down(void)
+{
+  return TOPICS_SHOW;
+}
+
 State topics(void)
 {
   switch (subState)
     {
     case TOPICS_SHOW:
+      subState=topics_show();
       break;
     case TOPICS_UP:
+      subState=topics_up();
       break;
     case TOPICS_DOWN:
+      subState=topics_down();
       break;
     case TOPICS_DONE:
       return DONE;
