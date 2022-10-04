b3d3eee0d6e8acd8ced3ac138d1b0e13daa74316
Thomas <thom.cherryhomes@gmail.com>
2022-10-04 00:06:27 -0500

    add articles scaffold.

--- news/apple2/src/articles.c
+++ news/apple2/src/articles.c
@@ -14,19 +14,49 @@
 
 static ArticlesState subState=ARTICLES_SHOW;
 
+ArticlesState articles_show(void)
+{
+  return ARTICLES_SHOW;
+}
+
+ArticlesState articles_up(void)
+{
+  return ARTICLES_SHOW;
+}
+
+ArticlesState articles_down(void)
+{
+  return ARTICLES_SHOW;
+}
+
+ArticlesState articles_page_up(void)
+{
+  return ARTICLES_SHOW;
+}
+
+ArticlesState articles_page_down(void)
+{
+  return ARTICLES_SHOW;
+}
+
 State articles(void)
 {
   switch (subState)
     {
     case ARTICLES_SHOW:
+      subState=articles_show();
       break;
     case ARTICLES_UP:
+      subState=articles_up();
       break;
     case ARTICLES_DOWN:
+      subState=articles_down();
       break;
     case ARTICLES_PAGE_UP:
+      subState=articles_page_up();
       break;
     case ARTICLES_PAGE_DOWN:
+      subState=articles_page_down();
       break;
     case ARTICLES_DONE:
       return DONE;
