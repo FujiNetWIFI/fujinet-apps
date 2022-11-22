cd87db3e3a1d332a9d937d4297f0e96ec6befef1
Thomas <thom.cherryhomes@gmail.com>
2022-11-22 15:58:19 -0600

    add dumb-typewriter

---
+++ dumb-typewriter/src/dumbtype.c
@@ -0,0 +1,34 @@
+/**
+ * DUMB Typewriter - Test the printer device
+ *
+ * @author  Thomas Cherryhomes
+ * @email   thom dot cherryhomes at gmail dot com
+ * @license gpl v. 3
+ */
+
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+#include "sp.h"
+
+void main(void)
+{
+  unsigned char ptr;
+
+  clrscr();
+
+  sp_init();
+  
+  ptr = sp_find_printer();
+  ptr = 10;
+
+  printf("Printer is dev %u\n\n",ptr);
+
+  printf("DUMB TYPEWRITER - <RETURN> SENDS TO PTR\n\n");
+
+  while(1)
+    {
+      gets((char *)sp_payload);
+      sp_write(ptr,strlen((char *)sp_payload));
+    }
+}
