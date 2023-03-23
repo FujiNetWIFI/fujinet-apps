695d1c335eaff52182a6be4b68d921b6330b3c65
Norman Davie <ecompservices@gmail.com>
2023-03-23 14:00:19 -0300

    Makefile now create netcat.po instead of distpo isConnected, if it were used, now returns the correct result all the smartport commands now use the correct dispatch address

--- netcat/apple2/src/main.c
+++ netcat/apple2/src/main.c
@@ -16,13 +16,14 @@ void banner(void)
 {
   clrscr();
   revers(1);
-  cprintf(" WELCOME TO NETCAT ");
+  cprintf(" WELCOME TO NETCAT");
   revers(0);
   cprintf("\r\n\r\n");
 }
 
 bool getURL(void)
 {
+  
   cprintf("ENTER NETWORK URL:\r\n>> ");
   gets(url);
 
@@ -67,7 +68,7 @@ void connect(void)
 bool isConnected(void)
 {
   sp_status(net,'S');
-  return sp_payload[2] & 0x02; // Bit 2 = Connected?
+  return sp_payload[2]; 
 }
 
 void in()
@@ -112,10 +113,12 @@ bool done(void)
 void main(void)
 {
   videomode(VIDEOMODE_80x24);
+
+  banner();
+
   sp_init();
   net = sp_find_network();
 
-  banner();
   
   cprintf("NET DEV IS %d\r\n",net);
   
@@ -125,7 +128,9 @@ void main(void)
   
   connect();
 
-  cprintf("CONNECTED.\n");
+  cprintf("CONNECTED to ");
+  cprintf(url);
+  cprintf(".\n");
   
   do 
     {
