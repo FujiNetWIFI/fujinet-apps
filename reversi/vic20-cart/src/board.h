3963aa91424bb676ad1c2307f5676a1c8b05a67b
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2023-06-07 21:52:35 -0500

    [vic20] reversi add board.h

---
+++ reversi/vic20-cart/src/board.h
@@ -0,0 +1,18 @@
+/**
+ * @brief #FujiNet Reversi
+ * @author  Thomas Cherryhomes
+ * @email   thom dot cherryhomes at gmail dot com
+ * @license gpl v. 3
+ */
+
+#ifndef BOARD_H
+#define BOARD_H
+
+typedef enum _disc
+  {
+    NONE,
+    BLACK,
+    WHITE
+  } Disc;
+
+#endif /* BOARD_H */
