4b30b2cf9bc22592ac8966e1d37a4e15529e56a0
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2023-06-08 13:59:13 -0500

    [vic20] reversi board logic complete, now debugging.

--- reversi/vic20-cart/src/board.c
+++ reversi/vic20-cart/src/board.c
@@ -31,7 +31,7 @@ unsigned short board_pos[64]=
     };
 
 /**
- * @brief initial board video RAM
+ * @brief initial board video RAM (as screen codes)
  */
 char board_char[] =
   {
@@ -64,6 +64,9 @@ char board_char[] =
     0x20,0x20,0x4A,0x46,0x46,0x46,0x46,0x46,0x46,0x46,0x46,0x46,0x46,0x46,0x46,0x46,0x46,0x46,0x46,0x49,0x20,0x20    
   };
 
+/**
+ * @brief footer display (as screen codes)
+ */
 const char footer[]={0xA0,0xA0,0xA0,0xA3,0x86,0x95,0x8A,0x89,0x8E,0x85,0x94,0xA0,0x92,0x85,0x96,0x85,0x92,0x93,0x89,0xA0,0xA0,0xA0};
 
 /**
@@ -97,12 +100,12 @@ Disc board_get_disc(Position p)
 
 /**
  * @brief place a piece on board
- * @param i board position to place piece
+ * @param p board position to place piece
  * @param d Disc color
  */
-void board_place_disc(Position i, Disc d)
+void board_place_disc(Position p, Disc d)
 {
-  unsigned short o = board_pos[i];
+  unsigned short o = board_pos[p];
   char s[3];
   
   // Set the characters
@@ -141,26 +144,144 @@ void board_place_disc(Position i, Disc d)
  * @param o opponent piece
  * @return true = we can flip, otherwise false.
  */
-bool walkBoard(Position p, signed char dx, signed char dy, Disc m, Disc o)
+bool board_walk(Position p, signed char dx, signed char dy, Disc m, Disc o)
 {
-  
+  int x,y;
+
+  // Convert position to x,y coordinates to make edge checking easier
+  y = p / BOARD_SIZE;
+  x = p % BOARD_SIZE;
+
+  if (board_get_disc(p) == o)
+    {
+      while ((x >= 0) && (x < 8) && (y >= 0) && (y < 8))
+	{
+	  x += dx;
+	  y += dy;
+	  p = dy * BOARD_SIZE + dx;
+	  
+	  if (board_get_disc(p) == NONE)
+	    return false;
+	  else if (board_get_disc(p) == m)
+	    return true;
+	}
+    }
+  else
+    {
+      // Opponent disc, keep walking.
+    }
+
+  // Either no consecutive opponent disc, or hit the edge;
+  return false;
 }
 
 /**
- * @brief check for valid move
+ * @brief assuming valid move, flip discs until there are no more opponent discs in direction.
  * @param p board position
- * @param d Disc piece (WHITE | BLACK)
+ * @param dx delta x
+ * @param dy delta y
+ * @param m my piece
+ * @param o opponent piece
  */
-void board_valid_move(Position p, Disc d)
+void board_flip(Position p, signed char dx, signed char dy, Disc m, Disc o)
 {
-  Disc opponent=WHITE;
+  int x,y;
 
-  if (d == WHITE)
-    d = BLACK;
+  // Convert position to x,y coordinates to make edge checking easier
+  y = p / BOARD_SIZE;
+  x = p % BOARD_SIZE;
 
+  while (board_get_disc(p) == o)
+    {
+      board_place_disc(p,m);
+      x += dx;
+      y += dy;
+      p = dy * BOARD_SIZE + dx;
+    }
+}
+
+/**
+ * @brief Check for valid move in all directions, set bit.
+ * @param p selected board position
+ * @param m color of my piece
+ * @param o color of opponent piece
+ * @return bit 0 = left, bit 1 = right, bit 2 = down, bit 3 = up
+ *         bit 4 = down-left, bit 5 = down-right, bit 6 = up-left, bit 7 = up-right
+ */
+ValidMoves board_check_move(Position p, Disc m, Disc o)
+{
+  unsigned char ret = 0;
+
+  // Check left
+  if (board_walk(p,-1,0,m,o))
+    ret |= 0x01;
+
+  // Check right
+  if (board_walk(p,-1,0,m,o))
+    ret |= 0x02;
+
+  // Check down
+  if (board_walk(p,-1,0,m,o))
+    ret |= 0x04;
+
+  // Check up
+  if (board_walk(p,-1,0,m,o))
+    ret |= 0x08;
+
+  // Check down-left
+  if (board_walk(p,-1,0,m,o))
+    ret |= 0x10;
+
+  // Check down-right
+  if (board_walk(p,-1,0,m,o))
+    ret |= 0x20;
+
+  // Check up-left
+  if (board_walk(p,-1,0,m,o))
+    ret |= 0x40;
+
+  // Check up-right
+  if (board_walk(p,-1,0,m,o))
+    ret |= 0x80;
+
+  return ret;
+}
+
+/**
+ * @brief attempt board move
+ * @param p Position to try
+ * @param m color of my disc
+ * @param o color of opponent disc
+ * @return true if move successful, otherwise false.
+ */
+bool board_move(Position p, Disc m, Disc o)
+{
+  ValidMoves v;
+
+  v = board_check_move(p,m,o);
+
+  /* Return if no valid moves */
+  if (!v)
+    return false;
+
+  if (v & 0x01) // left
+    board_flip(p-1,-1,0,m,o);
+  if (v & 0x02) // right
+    board_flip(p+1,1,0,m,o);
+  if (v & 0x04) // down
+    board_flip(p+BOARD_SIZE,0,-1,m,o);
+  if (v & 0x08) // up
+    board_flip(p-BOARD_SIZE,0,1,m,o);
+  if (v & 0x10) // down-left
+    board_flip(p+BOARD_SIZE-1,-1,1,m,o);
+  if (v & 0x20) // down-right
+    board_flip(p+BOARD_SIZE+1,1,1,m,o);
+  if (v & 0x40) // up-left
+    board_flip(p-BOARD_SIZE-1,-1,-1,m,o);
+  if (v & 0x80) // up right
+    board_flip(p-BOARD_SIZE+1,1,1,m,o);
   
-
-  
+  return true;
 }
 
 /**
@@ -169,12 +290,12 @@ void board_valid_move(Position p, Disc d)
 void board_reset(void)
 {
   // Reset color ram
-  memset(COLOR_RAM,0x0D,sizeof(board_char));
+  memset(COLOR_RAM,COLOR_NONE,sizeof(board_char));
 
   // and place board background characters
   memcpy(VIDEO_RAM,board_char,sizeof(board_char));
 
-  // Now place initial pieces
+  // Now place initial discs
   board_place_disc(START_POS_1,BLACK);
   board_place_disc(START_POS_2,WHITE);
   board_place_disc(START_POS_3,WHITE);
@@ -187,8 +308,6 @@ void board_reset(void)
 
 void main(void)
 {
-  unsigned char i;
-  
   video_setup();
 
   VIC.bg_border_color=88;  // Green with black border
@@ -197,6 +316,6 @@ void main(void)
   clrscr();
 
   board_reset();
-    
+  
   while(1);
 }
--- reversi/vic20-cart/src/board.h
+++ reversi/vic20-cart/src/board.h
@@ -15,7 +15,27 @@ typedef enum _disc
     WHITE
   } Disc;
 
+/**
+ * @brief a board position scanned left right, top bottom, 0-63
+ */
 typedef unsigned short Position; 
+
+/**
+ * @brief store returned valid moves
+ * @verbose bit 0 = left
+ *          bit 1 = right
+ *          bit 2 = down
+ *          bit 3 = up
+ *          bit 4 = down-left
+ *          bit 5 = down-right
+ *          bit 6 = up-left
+ *          bit 7 = up-right
+ *          no bits = no valid moves
+ */
+typedef unsigned char ValidMoves;
+
+/* symmetrical board size in rows/columns */
+#define BOARD_SIZE 8
 
 #define DISC_CHAR_1 0x3B
 #define DISC_CHAR_2 0x3C
@@ -29,7 +49,13 @@ typedef unsigned short Position;
 
 #define FOOTER_OFFSET 484
 
+/* Color RAM value for empty pieces */
+#define COLOR_NONE 0x0D
+
 /* bias value to convert Disc value to color ram value */
 #define COLOR_RAM_BIAS 7
+
+#define SCREEN_SIZE_X 22
+#define SCREEN_SIZE_Y 23
 
 #endif /* BOARD_H */
