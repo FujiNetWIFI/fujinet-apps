ca9499b33d8734de4bf8aa643443f6185146abe5
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2023-07-03 13:13:21 -0500

    [5cardstud][vic20] add scaffold.

---
+++ 5cardstud/vic20/src/main.c
@@ -0,0 +1,13 @@
+/**
+ * @brief   5-card-stud
+ * @author  Eric Carr, Thomas Cherryhomes, (insert names here)
+ * @license gpl v. 3
+ * @verbose main
+ */
+
+#include <vic20.h>
+
+void main(void)
+{
+  
+}
