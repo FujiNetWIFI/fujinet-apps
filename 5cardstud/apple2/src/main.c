a0ec2e743f21484b3d22f67ba2f31bdf5626fa78
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2023-07-04 15:17:41 -0500

    [5cardstud][apple2] janky little poker chip.

--- 5cardstud/apple2/src/main.c
+++ 5cardstud/apple2/src/main.c
@@ -4,6 +4,7 @@
 #include "background.h"
 #include "charset.h"
 #include "text.h"
+#include "pot.h"
 
 int i;
 
@@ -13,24 +14,7 @@ void main(void)
   background();
   status("HI ERIC IT WORKS YEAH\nSECOND LINE");
 
-  hires_putc(17,96,ROP_AND(0x55),';');
-  hires_putc(18,96,ROP_AND(0x2A),'@');
-  hires_putc(19,96,ROP_AND(0x55),'@');
-  hires_putc(20,96,ROP_AND(0x2A),'@');
-  hires_putc(21,96,ROP_AND(0x55),'@');
-  hires_putc(22,96,ROP_AND(0x2A),'<');
-  hires_putc(17,104,ROP_AND(0x55),'?');
-  hires_putc(22,104,ROP_AND(0x2A),'?');
-  hires_putc(17,112,ROP_AND(0x55),'=');
-  hires_putc(18,112,ROP_AND(0x2A),'@');
-  hires_putc(19,112,ROP_AND(0x55),'@');
-  hires_putc(20,112,ROP_AND(0x2A),'@');
-  hires_putc(21,112,ROP_AND(0x55),'@');
-  hires_putc(22,112,ROP_AND(0x2A),'>');
-
-  hires_putc(19,104,ROP_OR(0x55),'1');
-  hires_putc(20,104,ROP_OR(0x2A),'0');
-  hires_putc(21,104,ROP_OR(0x55),'0');
+  pot(100);
   
   cgetc();
   hires_Done();
