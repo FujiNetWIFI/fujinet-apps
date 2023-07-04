d9deef7582af0896e9959a585a1f75087fc1f9e4
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2023-07-04 15:16:47 -0500

    [5cardstud][apple2] pot display into its own function.

---
+++ 5cardstud/apple2/src/pot.h
@@ -0,0 +1,14 @@
+/**
+ * @brief Pot display
+ */
+
+#ifndef POT_H
+#define POT_H
+
+/**
+ * @brief Display pot
+ * @param n The number to display in pot
+ */
+void pot(unsigned short n);
+
+#endif /* POT_H */
