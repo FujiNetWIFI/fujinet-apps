6babbec3973d1229150e85272f5089cea2fbd893
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2023-07-03 21:24:47 -0500

    [5cardstud][apple2] now we're gettin' somewhere.

--- 5cardstud/apple2/src/main.c
+++ 5cardstud/apple2/src/main.c
@@ -2,14 +2,15 @@
 #include <conio.h>
 #include "hires.h"
 #include "background.h"
+#include "cards.h"
 
 unsigned i;
 
 void main(void)
 {
-
   hires_Init();
   background();
+  hires_Draw(0,0,1,8,ROP_CPY,&cards_bin[0]);
   cgetc();
   hires_Done();
 }
