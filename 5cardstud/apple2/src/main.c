cc856dce5476c56679df526bdb5481d32248d619
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2023-07-04 18:11:50 -0500

    [5cardstud][apple2] i am exhausted.

--- 5cardstud/apple2/src/main.c
+++ 5cardstud/apple2/src/main.c
@@ -5,16 +5,25 @@
 #include "charset.h"
 #include "text.h"
 #include "pot.h"
+#include "card.h"
 
 int i;
 
 void main(void)
 {
+  unsigned char x,y;
+  
   hires_Init();
   background();
   status("HI ERIC IT WORKS YEAH\nSECOND LINE");
 
   pot(100);
+
+  // /////////////////////////////////////////////
+  
+  card(18,140,CARD_2,SUIT_SPADE);
+
+  // /////////////////////////////////////////////
   
   cgetc();
   hires_Done();
