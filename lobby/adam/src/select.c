eab642163bdf3f183d46677e21563eba447b7e9c
Thomas <thom.cherryhomes@gmail.com>
2023-08-22 15:43:37 -0500

    [lobby][adam] start on select.

--- lobby/adam/src/select.c
+++ lobby/adam/src/select.c
@@ -5,9 +5,30 @@
  * @license gpl v. 3, see LICENSE for details.
  */
 
+#include <msx.h>
+#include <smartkeys.h>
 #include "select.h"
+#include "state.h"
+#include "input.h"
+
+extern State state;
 
 void select(void)
 {
+  smartkeys_display(NULL,NULL," CHANGE\n  NAME"," SHOUT","  CHAT"," REFRESH");
+  smartkeys_status("\n  SELECT A ROOM\n");
+  smartkeys_sound_play(SOUND_MODE_CHANGE);
 
+  while (state==SELECT)
+    {
+      switch(input())
+	{
+	case KEY_UP_ARROW:
+	  break;
+	case KEY_DOWN_ARROW:
+	  break;
+	}
+    }
+  
+  state=HALT;
 }
