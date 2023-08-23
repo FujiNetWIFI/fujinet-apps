fc1b848911f98a56f1a33ca8003e7f4b8cf74238
Thomas <thom.cherryhomes@gmail.com>
2023-08-22 19:48:39 -0500

    [lobby][adam] implement connect state for chat server.

---
+++ lobby/adam/src/connect.h
@@ -0,0 +1,13 @@
+/**
+ * @brief FujiNet Lobby for Adam
+ * @author Thomas Cherryhomes
+ * @email thom dot cherryhomes at gmail dot com
+ * @license gpl v. 3, see LICENSE for details.
+ */
+
+#ifndef CONNECT_H
+#define CONNECT_H
+
+void connect(void);
+
+#endif /* CONNECT_H */
