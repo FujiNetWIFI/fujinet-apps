b46c6cb4e499481f8bba98083bd244c69e32469b
Leif Bloomquist <github@leifbloomquist.net>
2023-12-08 15:36:44 -0500

    Added lookup tables for lat/lon and optimized redraw

--- iss-tracker/vic20-5k/src/satellite.c
+++ iss-tracker/vic20-5k/src/satellite.c
@@ -1,21 +1,57 @@
 /**
  * @brief   ISS Tracker for 5 kilobyte VIC-20
- * @author  Thomas Cherryhomes
+ * @author  Thomas Cherryhomes, Leif Bloomquist
  * @email   thom dot cherryhomes at gmail dot com
  * @license gpl v. 3, see COPYING for details
+ *
+ * LB: Added lookup tables for lat/lon
  */
-
-#define X_CENTER 11
-#define Y_CENTER 11
 
 #include <conio.h>
 #include "video.h"
 
+const unsigned short longitude_to_x[361] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21 };
+const unsigned char  latitude_to_y[181] = { 18, 18, 18, 18, 18, 18, 18, 18, 18, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 16, 16, 16, 16, 16, 16, 16, 16, 16, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 14, 14, 14, 14, 14, 14, 14, 14, 14, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 12, 12, 12, 12, 12, 12, 12, 12, 12, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 10, 10, 10, 10, 10, 10, 10, 10, 10, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 8, 8, 8, 8, 8, 8, 8, 8, 8, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 6, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 4, 4, 4, 4, 4, 4, 4, 4, 4, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+#define LONGITUDE_LIMIT 180
+#define LATITUDE_LIMIT  90
+
+/**
+ * @brief Convert longitude to X coordinate
+ * @param lon Longitude
+ * @return X coordinate
+ */
+unsigned short lon_to_x(int lon)
+{
+  if (lon < -LONGITUDE_LIMIT) lon = -LONGITUDE_LIMIT;
+  if (lon >  LONGITUDE_LIMIT) lon =  LONGITUDE_LIMIT;
+
+  return longitude_to_x[LONGITUDE_LIMIT + lon];
+}
+
+/**
+ * @brief Convert latitude to Y coordinate
+ * @param lat Longitude
+ * @return Y coordinate
+ */
+unsigned char lat_to_y(int lat)
+{
+  if (lat < -LATITUDE_LIMIT) lat = -LATITUDE_LIMIT;
+  if (lat >  LATITUDE_LIMIT) lat =  LATITUDE_LIMIT;
+
+  return latitude_to_y[LATITUDE_LIMIT + lat];
+}
+
+/**
+ * @brief Display satellite at lon, lat
+ * @param lon Integer longitude (-180, 180 deg)
+ * @param lat Integer latitude  (-90, 90 deg)
+ */
 void satellite(int lon, int lat)
 {
-  unsigned char x = X_CENTER + (unsigned char) ( lon >> 4 );
-  unsigned char y = Y_CENTER - (unsigned char) ( lat >> 4 );
-
+  unsigned short x = lon_to_x(lon);
+  unsigned char  y = lat_to_y(lat);
+  
   gotoxy(x,y);
   print("\x1c\xD1\x1f");
 }
