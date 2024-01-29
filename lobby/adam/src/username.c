11fe154fb60dad9f97415f6c874ece743423a197
Geoff Oltmans <oltmansg@gmail.com>
2024-01-28 23:56:28 -0600

    a little cleanup

--- lobby/adam/src/username.c
+++ lobby/adam/src/username.c
@@ -34,8 +34,7 @@ void username_set(void)
 
 void username_get(void)
 {
-//  strcpy(username,response);
-//  state=CONNECT;
+  state=CONNECT;
 }
 
 void username(void)
@@ -46,5 +45,5 @@ void username(void)
   if (_username[0]==0x00)
     username_set();
   else
-    state=CONNECT;
+    username_get();
 }
