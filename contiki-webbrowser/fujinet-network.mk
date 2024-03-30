d8552bf46c97033d61ef221d7dbb98be69163b96
Oliver Schmidt <ol.sc@web.de>
2024-03-30 18:24:51 +0100

    Use Makefile from fujinet-lib-examples as template

--- contiki-webbrowser/fujinet-network.mk
+++
@@ -1,6 +0,0 @@
-CFLAGS += -I ../fn_network
-
-contiki-webbrowser.apple2 \
-contiki-webbrowser.apple2enh: $(wildcard ../fn_network/fujinet-network-apple2_*.lib)
-
-contiki-webbrowser.atari: $(wildcard ../fn_network/fujinet-network-atari_*.lib)
