f02f28742e7801abf7fd80b58d85ee89d76fe25b
Mark Fisher <mark.j.fisher@gmail.com>
2024-04-03 09:12:34 +0100

    Add httpbin and 5cs json tests - tls check

--- fujinet-lib-examples/json-test/src/jsontest.c
+++ fujinet-lib-examples/json-test/src/jsontest.c
@@ -25,6 +25,13 @@ char longitude_query[]  = "/iss_position/longitude";
 char latitude_query[]   = "/iss_position/latitude";
 char timestamp_query[]  = "/TIMESTAMP";
 
+char five_card_stud_url[] = "n:https://5card.carr-designs.com/state?table=blue&player=THOMC";
+char round_query[] = "/round";
+char pot_query[] = "/pot";
+
+char httpbin_url[] = "n:https://httpbin.org/get";
+char host_query[] = "/headers/host";
+
 char result[1024];
 uint8_t err = 0;
 char *url;
@@ -32,24 +39,70 @@ uint16_t conn_bw;
 uint8_t connected;
 uint8_t conn_err;
 
-char *version = "v2.0.2";
+bool sure = false;
+
+char *version = "v2.1.0";
 
 int main(void) {
     setup();
 
     printf("jsontest %s\n", version);
-    printf("Base URL: %s\n\n", iss_now);
 
-    start_get();                        // save us having to keep closing/reopening.
-    test_get_query(longitude_query);
-    test_get_query(latitude_query);
-    test_get_query(timestamp_query);
-    end_get();                          // finally close resource
+    test_iss_json();
+    test_httpbin_json();
+    test_5cs_json();
 
     printf("press a key to exit.");
     cgetc();
 
    return 0;
+}
+
+bool ask() {
+  char sure = 0;
+  printf("Run this test? ");
+  sure = cgetc();
+  if (sure == 'y' || sure == 'Y') {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+void test_iss_json() {
+    printf("ISS JSON Test\n");
+    sure = ask();
+    if (!sure) return;
+    printf("\n");
+
+    start_get(iss_now);
+    test_get_query(longitude_query);
+    test_get_query(latitude_query);
+    test_get_query(timestamp_query);
+    end_get();
+}
+
+void test_httpbin_json() {
+    printf("Httpbin Test\n");
+    sure = ask();
+    if (!sure) return;
+    printf("\n");
+
+    start_get(httpbin_url);
+    test_get_query(host_query);
+    end_get();
+}
+
+void test_5cs_json() {
+    printf("5 Card Stud Test\n");
+    sure = ask();
+    if (!sure) return;
+    printf("\n");
+
+    start_get(five_card_stud_url);
+    test_get_query(round_query);
+    test_get_query(pot_query);
+    end_get();
 }
 
 void handle_err(char *reason) {
@@ -62,12 +115,12 @@ void handle_err(char *reason) {
 }
 
 void setup() {
-    url = iss_now;
     bzero(result, 1024);
     gotox(0);
 }
 
-void start_get() {
+void start_get(char *u) {
+    url = u;
     err = network_open(url, OPEN_MODE_HTTP_GET, OPEN_TRANS_NONE);
     handle_err("open");
 
--- fujinet-lib-examples/json-test/src/jsontest.h
+++ fujinet-lib-examples/json-test/src/jsontest.h
@@ -2,6 +2,7 @@
 #define JSONTEST_H
 
 #include <stdint.h>
+#include <stdbool.h>
 
 void handle_err(char *reason);
 void setup();
@@ -10,5 +11,9 @@ void start_get();
 void end_get();
 
 void test_get_query(char *path);
+void test_iss_json();
+void test_5cs_json();
+void test_httpbin_json();
+bool ask();
 
 #endif
