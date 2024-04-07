d27e3a2e8e5e619dca3ab88a31702107ae2bb7c3
Oliver Schmidt <ol.sc@web.de>
2024-04-07 15:28:52 +0200

    Distinguish mime types and support redirects and https

--- contiki-webbrowser/src/webclient.c
+++ contiki-webbrowser/src/webclient.c
@@ -37,12 +37,16 @@ webclient_open(const char *url)
 
   strncpy(s.device + 2, url, WWW_CONF_MAX_URLLEN);
 
-  if(network_open(s.device, OPEN_MODE_HTTP_GET, OPEN_TRANS_NONE) != FN_ERR_OK) {
+  if(network_open(s.device, OPEN_MODE_HTTP_GET_H, OPEN_TRANS_NONE) != FN_ERR_OK) {
     return 0;
   }
   s.open = 1;
+  s.mimetype[0] = '\0';
 
-  strcpy(s.mimetype, "html");
+  network_http_set_channel_mode(s.device, HTTP_CHAN_MODE_COLLECT_HEADERS);
+  network_http_add_header(s.device, "content-type\x9b");
+  network_http_add_header(s.device, "location\x9b");
+  network_http_set_channel_mode(s.device, HTTP_CHAN_MODE_BODY);
 
   webclient_connected();
 
@@ -83,6 +87,26 @@ webclient_exit(void)
 {
 }
 /*-----------------------------------------------------------------------------------*/
+static int
+read_header(char *buffer, int buflen)
+{
+  uint16_t len;
+  uint8_t c;
+  uint8_t err;
+
+  network_status(s.device, &len, &c, &err);
+  if(len < 2 || len > buflen) {
+    return 0;
+  }
+  len = network_read(s.device, buffer, len);
+  if(len < 2) {
+    return 0;
+  }
+  /* Last char is 0x9b, convert to NUL */
+  buffer[len - 1] = '\0';
+  return 1;
+}
+/*-----------------------------------------------------------------------------------*/
 static void
 webclient_poll(void)
 {
@@ -94,12 +118,33 @@ webclient_poll(void)
 
   retval = network_read_nb(s.device, s.buffer, sizeof(s.buffer));
 
-  if (retval > 0) {
+  if(retval > 0) {
+
+    /* Read headers if not done yet */
+    if(s.mimetype[0] == '\0') {
+      char *url = www_geturl();
+
+      network_http_set_channel_mode(s.device, HTTP_CHAN_MODE_GET_HEADERS);
+
+      /* content-type */
+      if(!read_header(s.mimetype, sizeof(s.mimetype))) {
+        webclient_close();
+        return;
+      }
+
+      /* location */
+      if(read_header(url, WWW_CONF_MAX_URLLEN)) {
+        www_seturl();
+      }
+
+      network_http_set_channel_mode(s.device, HTTP_CHAN_MODE_BODY);
+    }
+
     webclient_datahandler(s.buffer, retval);
   }
 
   /* Check if we hit EOF, i.e. no more data for this URL */
-  if (fn_network_error == 136) {
+  if(fn_network_error == 136) {
     network_close(s.device);
     s.open = 0;
 
