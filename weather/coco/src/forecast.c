29cc1a0a2835fc2b8853cd1c1fa986da94798db2
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2024-04-26 17:44:26 -0500

    [coco][weather] clean up.

--- weather/coco/src/forecast.c
+++ weather/coco/src/forecast.c
@@ -9,9 +9,6 @@
 #include <cmoc.h>
 #include <coco.h>
 #include "gfx.h"
-#include "thunderstorm.h"
-#include "clear.h"
-#include "rain.h"
 
 void forecast(int slot,
 	      const char *day0,
@@ -36,62 +33,3 @@ void forecast(int slot,
   puts(slot+0+2,140+2,0,wind0);
 }
 
-/**
- * @brief Test harness, remove.
- */
-int main(void)
-{
-  initCoCoSupport(0);
-  rgb();
-  width(32);
-  gfx(1);
-  gfx_cls(CYAN);
-
-  forecast(0,
-	   "31",
-	   "Dec",
-	   "Sun",
-	   "44",
-	   "58",
-	   "30\x1e\x1f",
-	   "\x1d\x31\x35m",
-	   "H: 100%",
-	   thunderstorm);
-  
-  forecast(1,
-	   "1",
-	   "Jan",
-	   "Mon",
-	   "40",
-	   "45",
-	   "30\x1e\x1f",
-	   "\x1d\x31\x33m",
-	   "H: 100%",
-	   clear);
-
-  forecast(2,
-	   "2",
-	   "Jan",
-	   "Tue",
-	   "43",
-	   "47",
-	   "30\x1e\x1f",
-	   "\x1d\x31\x33m",
-	   "H: 100%",
-	   rain);
-
-  forecast(3,
-	   "3",
-	   "Jan",
-	   "Wed",
-	   "47",
-	   "50",
-	   "30\x1e\x1f",
-	   "\x1d\x31\x33m",
-	   "H: 100%",
-	   rain);
-
-  while(1);
-
-  return 0;
-}
