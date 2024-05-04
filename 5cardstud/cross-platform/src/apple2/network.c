b98e8e8728edbfc4b15f3817f9957ee674fac473
Eric Carr <eric.carr@gmail.com>
2024-05-03 22:57:03 -0500

    5CS Cross Platform WIP

--- 5cardstud/cross-platform/src/apple2/network.c
+++ 5cardstud/cross-platform/src/apple2/network.c
@@ -8,13 +8,19 @@
 #include <conio.h>
 #include <stdbool.h>
 #include "../platform-specific/appkey.h"
+#include "../fujinet-fuji.h"
+#include "../fujinet-network.h"
 
 /// @brief Retrieve a json parsed url response
 /// @param url 
 /// @return response length
 int getJsonResponse(char *url, unsigned char *buffer, int max_len) {
-  static int i, len;
-  return len;
+  int err,count = 0;
+  err = network_open(url, OPEN_MODE_HTTP_GET, OPEN_TRANS_NONE);
+  err = network_json_parse(url);
+  count = network_json_query(url, "", buffer);
+  err = network_close(url);
+  return count;
 }
 
 #endif /* __APPLE2__ */
