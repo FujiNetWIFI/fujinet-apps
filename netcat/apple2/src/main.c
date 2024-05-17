1b8372341f73ec14158cde09aa666b9d739f3b18
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2024-05-17 13:42:59 -0500

    [netcat][apple2] main to use text, simplify banner.

--- netcat/apple2/src/main.c
+++ netcat/apple2/src/main.c
@@ -15,32 +15,34 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-uint8_t rxbuf[8192];
+uint8_t rxbuf[8192];     // Receive buffer
+char devicespec[256];    // Buffer for the device spec
+uint16_t bw;             // # of bytes waiting (0-65535)
+uint8_t c;               // Connected? (0=false, 1=true)
+uint8_t err;             // Error code from most recent status
 
-char devicespec[256];
-uint16_t bw;
-uint8_t c;
-uint8_t err;
+const char *banner =
+//   ---------------------------------------|----------------------------------------
+    "Welcome to FujiNet NetCat, Version 2.0   A Dumb Terminal Emulator\n\n"
+    "Enter a URL in the form:\n\n"
+    "N:PROTO://HOST.NAME:PORT/[PATH...]\n\n"
+    "Some Protocols: TCP, UDP, HTTP/S, SSH,  FTP, TNFS, TELNET\n\n"
+    "URL Examples:\n\n"
+    "N:TELNET://BBS.FOZZTEXX.COM             N:TCP://IRATA.ONLINE:6502/\n"
+    "N:SSH://MYLINUXHOST/\n"
+    "\n\n"
+    "ENTER URL\n\n"
+    ">> ";
 
 bool connect(void)
 {
+    init80();
     network_init();
+
     clrscr();
     
-    outs("WELCOME TO #FUJINET NETCAT V2.0\n");
-    outs("-------------------------------\n\n");
-
-    outs("ENTER A URL IN THE FORM:\n\n");
-    outs("N:PROTO://HOST.NAME:PORT/[PATH...]\n\n");
-
-    outs("EXAMPLES OF N: URLs:\n");
-    outs("--------------------\n");
-    outs("N:TELNET://BBS.FOZZTEXX.COM/\n");
-    outs("N:TCP://IRATA.ONLINE:6502/\n");
-    outs("N:SSH://MYLINUXHOST/\n");
-    outs("N:HTTPS://www.gnu.org/licenses/gpl-3.0.txt\n\n");
-
-    cursor(1);
+    outs(banner);
+    
     read_line(devicespec,sizeof(devicespec),false);
 
     outs("\n\n");
@@ -75,15 +77,13 @@ void in(void)
 
         network_read(devicespec, rxbuf, bw);
 
-        outc(0x08);
+        outc(CH_CURS_LEFT);
 
         for (i=0;i<bw;i++)
             if (rxbuf[i]!=0x0D)
                 outc(rxbuf[i]);
 
-        revers(1);
-        outc(0x20);
-        revers(0);
+        place_cursor();
     }
 }
 
@@ -111,9 +111,7 @@ int main(void)
         esc_to_quit_or_restart();
     }
 
-    revers(1);
-    outc(0x20);
-    revers(0);
+    place_cursor();
     
     while (is_connected())
         netcat();
