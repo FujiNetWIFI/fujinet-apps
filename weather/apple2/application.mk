63aca9fa8d3cabcc47b8d280f37c643a1b169b34
Andrew Diller <dillera@dillernet.com>
2024-06-25 20:37:47 -0400

    added more cflags to application.mk

--- weather/apple2/application.mk
+++ weather/apple2/application.mk
@@ -4,4 +4,5 @@
 # reserved memory for graphics
 # LDFLAGS += -Wl -D,__RESERVED_MEMORY__=0x2000
 
-LDFLAGS += -C src/apple2-hgrtext.cfg
+CFLAGS = -Os -DBUILD_APPLE2 --include-dir src/common
+LDFLAGS += -C src/apple2-hgrtext.cfg -m $(PROGRAM).map --ld-args -D,__HIMEM__=0xBF00 apple2-iobuf-0800.o
