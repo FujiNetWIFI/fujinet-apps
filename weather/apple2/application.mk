af9a5f28d21a827165f09685b2a1bb4e3010e43a
Andrew Diller <dillera@dillernet.com>
2024-06-25 20:25:24 -0400

    added local app.mk for apple2 build

---
+++ weather/apple2/application.mk
@@ -0,0 +1,7 @@
+################################################################
+# COMPILE FLAGS
+
+# reserved memory for graphics
+# LDFLAGS += -Wl -D,__RESERVED_MEMORY__=0x2000
+
+LDFLAGS += -C src/apple2-hgrtext.cfg
