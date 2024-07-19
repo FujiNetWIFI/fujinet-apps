68ee168a0379a479365836261691a7e0395d1d74
Bill Athing <bill.athing@gmail.com>
2024-07-19 00:38:11 -0500

    Integrate builds with cardgame and fujinet-lib

--- 5cardstud/cross-platform/src/coco/text.c
+++ 5cardstud/cross-platform/src/coco/text.c
@@ -6,6 +6,10 @@
  */
 //#include <string.h>
 #include "hires.h"
+#include "hires.h"
+
+
+void drawTextAt(unsigned char x, unsigned char y, char*s);
 
 #define CHAR_HEIGHT             8
 #define SCREEN_WIDTH            40
@@ -18,20 +22,21 @@ unsigned char rop_mod[] = {0x55,0x2A};
 
 void hires_double_buffer_draw()
 {
-  memcpy((void*)0x4000,(void*)0x2000,0x2000);
+//  memcpy((void*)0x4000,(void*)0x2000,0x2000);
 }
 
 /*-----------------------------------------------------------------------*/
 void hires_putc(unsigned char x, unsigned char y, unsigned rop, unsigned char c)
 {
-    hires_Draw(x,y,1,CHAR_HEIGHT,rop,&charset[c<<3]);
+//hires_Draw(x,y,1,CHAR_HEIGHT,rop,&charset[c<<3]);
+  drawTextAt(x,y,c);  
 }
 
 /*-----------------------------------------------------------------------*/
 void hires_putcc(unsigned char x, unsigned char y,unsigned rop, unsigned cc)
 {
-  hires_putc(x,y,rop,cc>>8);
-  hires_putc(++x,y,rop,cc);
+  drawTextAt(x,y,cc>>8);
+  drawTextAt(++x,y,cc);
 }
 
 #endif
