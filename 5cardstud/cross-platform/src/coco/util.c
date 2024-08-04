116826c358558b6481a4ec222883c111e011b80d
Bill Athing <bill.athing@gmail.com>
2024-08-04 14:03:21 -0700

    various coco cleanups

--- 5cardstud/cross-platform/src/coco/util.c
+++ 5cardstud/cross-platform/src/coco/util.c
@@ -1,6 +1,7 @@
 /*
   Platform specific utilities that don't fit in any category
 */
+#include <coco.h>
 
 void resetTimer()
 {
@@ -13,4 +14,30 @@ int getTime()
 
 void quit()
 {
+}
+
+static char lastchar;
+
+// lastchar is only set by kbhit, we're basically handling the case
+// where that key picked up by kbhit doesn't get lost.
+char cgetc() {
+  char c;
+  if (lastchar==0)
+  {
+    do{
+      c= inkey();
+    } while(c==0);
+    return c;
+  }
+  c= lastchar;
+  lastchar= 0;
+  return(c);
+}
+
+char kbhit() {
+  char c;
+
+  c= inkey();
+  lastchar= c;
+  return(c);
 }
