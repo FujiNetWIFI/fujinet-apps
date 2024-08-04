c9f703070ee7a35ea98dcc979f1f99ee067d4ec4
Bill Athing <bill.athing@gmail.com>
2024-08-04 11:56:21 -0700

    More files for cardgame lib

---
+++ 5cardstud/cross-platform/src/coco/lib/cardgame/compiled-cards.h
@@ -0,0 +1,59 @@
+/*  compiled-cards.h - Playing card bitmaps for the Color Computer.
+
+    By Pierre Sarrazin <http://sarrazip.com/>.
+    This file is in the public domain.
+
+    Version 0.1.1 - 2016-03-12 - First release in the public domain.
+    Version 0.1.2 - 201?-??-?? - Adapted to compile in linker mode.
+*/
+
+#ifndef _compiled_cards_h
+#define _compiled_cards_h
+
+
+extern const unsigned short redCard_1[10];
+extern const unsigned short redCard_2[10];
+extern const unsigned short redCard_3[10];
+extern const unsigned short redCard_4[10];
+extern const unsigned short redCard_5[10];
+extern const unsigned short redCard_6[10];
+extern const unsigned short redCard_7[10];
+extern const unsigned short redCard_8[10];
+extern const unsigned short redCard_9[10];
+extern const unsigned short redCard_10[10];
+extern const unsigned short redCard_11[10];
+extern const unsigned short redCard_12[10];
+extern const unsigned short redCard_13[10];
+extern const unsigned short blackCard_1[10];
+extern const unsigned short blackCard_2[10];
+extern const unsigned short blackCard_3[10];
+extern const unsigned short blackCard_4[10];
+extern const unsigned short blackCard_5[10];
+extern const unsigned short blackCard_6[10];
+extern const unsigned short blackCard_7[10];
+extern const unsigned short blackCard_8[10];
+extern const unsigned short blackCard_9[10];
+extern const unsigned short blackCard_10[10];
+extern const unsigned short blackCard_11[10];
+extern const unsigned short blackCard_12[10];
+extern const unsigned short blackCard_13[10];
+extern const unsigned short * const redCards[13];
+extern const unsigned short * const blackCards[13];
+extern const unsigned short suit_hearts[7];
+extern const unsigned short suit_spades[7];
+extern const unsigned short suit_diamonds[7];
+extern const unsigned short suit_clubs[7];
+extern const unsigned short * const suits[4];
+extern const unsigned short cardTopLeft[5];
+extern const unsigned short cardBottomLeft[5];
+extern const unsigned short cardTopRight[5];
+extern const unsigned short cardBottomRight[5];
+extern const unsigned short cardLeft[1];
+extern const unsigned short cardRight[1];
+extern const unsigned short redJokerValue[10];
+extern const unsigned short blackJokerValue[10];
+extern const unsigned short redJokerSuit[7];
+extern const unsigned short blackJokerSuit[7];
+
+
+#endif  /* _compiled_cards_h */
