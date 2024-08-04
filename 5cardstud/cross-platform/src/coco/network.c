547cbb6795d96eafc6a4b5b9b04fc3490ad9a58b
Bill Athing <bill.athing@gmail.com>
2024-08-04 14:03:21 -0700

    various coco cleanups

--- 5cardstud/cross-platform/src/coco/network.c
+++ 5cardstud/cross-platform/src/coco/network.c
@@ -18,6 +18,8 @@
 static uint8_t trans_type = OPEN_TRANS_CRLF;
 static uint8_t err = 0;
 
+#define NET_DEBUG 1
+
 void handle_err(char *message) {
 	if (err <  0) {
 		printf("err: %s\n", message);
@@ -28,24 +30,24 @@ void handle_err(char *message) {
 /// @param url 
 /// @return response length
 uint16_t getJsonResponse(char *url, unsigned char *buffer, uint16_t max_len) {
-  int count;
   static char buf[128];
   uint8_t c, err;
-  //char * new_url = "N:http://fujinet.online:8080";
-  //const char * req_fmt = ;
-  //char req[128];
-#if 0
-  static uint16_t count, bw;
+
+  static uint16_t count, bw, n;
+
+#ifdef NET_DEBUG  
   printf("URL: %s\n", url);
-printf("network open\n");
+  printf("network open\n");
+#endif
   network_open(url, OPEN_MODE_RW, trans_type);
 //  network_json_parse(url);
 //  net_set_channel_mode(0,0x01);
+#ifdef NET_DEBUG  
 printf("back from network open(press enter)\n");
 readline();    
 
 printf("network_status\n");
-//  for (i=0;i<4;i++)
+#endif
   {
   //  sprintf(req, "/view?platform=atari&pagesize=1&page=%d", i);
     buf[0]=0;
@@ -59,10 +61,12 @@ printf("network_status\n");
     {
       count= sizeof(buf);
     }
+#ifdef NET_DEBUG  
 printf("back from network network_status(press enter)\n");
 readline();    
-//getchar();    
+
 printf("network_read\n");
+#endif
     n = network_read(url,buf,count);
     if (n < 0) 
     {
@@ -71,23 +75,19 @@ printf("network_read\n");
     }
   //  count = network_json_query(new_url, req, buffer);
     printf("count = %d, %s\n", count, buf);
+#ifdef NET_DEBUG  
 printf("back from network read(press enter)\n");
 readline();    
+#endif
   }
-//getchar();    
+#ifdef NET_DEBUG  
 printf("network close\n");
+#endif
   network_close(url);
+#ifdef NET_DEBUG  
 printf("back from network close(press enter)\n");
 readline();    
-#else
-  uint16_t bw;
-
-        network_open(url,12,0);
-        network_status(url,&bw,&c,&err);
-        network_read(url,buf,bw);
-        network_close(url);
-
-#endif  
+#endif
   return count;
 }
 
