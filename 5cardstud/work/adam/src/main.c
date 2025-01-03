6bfac8ba5a0415e18fd01f17e3d0a34611654c9e
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2025-01-03 14:35:38 -0600

    5cs charset and playfield example

---
+++ 5cardstud/work/adam/src/main.c
@@ -0,0 +1,36 @@
+/**
+ * @brief Playfield test.
+ * @author Thom Cherryhomes
+ * @email thom dot cherryhomes at gmail dot com
+ * @license gpl v. 3, see LICENSE for details.
+ */
+
+#include <conio.h>
+#include <video/tms99x8.h>
+#include <sys/ioctl.h>
+#include <eos.h>
+#include <smartkeys.h>
+#include "udgs.h"
+
+void load_udgs()
+{
+    void *param = &udgs;
+    console_ioctl(IOCTL_GENCON_SET_UDGS,&param);
+}
+
+void main(void)
+{
+	smartkeys_set_mode();
+        load_udgs();
+        vdp_color(VDP_INK_WHITE,VDP_INK_DARK_GREEN,VDP_INK_DARK_GREEN);
+        clrscr();
+        
+        cputs("\xa0\xa1 \xa2\xa3\xa4"); // BET
+
+	cputs(" NORMAL TEXT");
+
+        smartkeys_puts(64,64,"Here is an example of SmartKeys Output.");
+
+
+	while(1);
+}
