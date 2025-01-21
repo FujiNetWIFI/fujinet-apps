687ab50166424426a3643aa2dd1d3287f1796b3e
Oliver Schmidt <ol.sc@web.de>
2025-01-21 19:49:16 +0100

    Removed nget & nput now that they live in fuji-tools.

--- nget/src/atari/local_file_init.c
+++
@@ -1,7 +0,0 @@
-/**
- * @brief do any local filesystem housekeeping
- */
-
-void local_file_init(void)
-{
-}
--- nput/src/common/util.c
+++
@@ -1,7 +0,0 @@
-#include "util.h"
-
-// This file is included in every compilation for every platform and target.
-
-uint8_t sum(uint8_t i, uint8_t j) {
-	return i + j;
-}
