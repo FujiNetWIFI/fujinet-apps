e445db72249fc7b3f9a8e78dc1f4e99487d6b393
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2025-04-27 14:46:00 -0500

    [fujitzee][adam] vars need changing.

--- fujitzee/cross-platform/src/adam/vars.h
+++ fujitzee/cross-platform/src/adam/vars.h
@@ -33,7 +33,8 @@
 #define KEY_ESCAPE       0x5F
 #define KEY_ESCAPE_ALT   0x03
 #define KEY_SPACE        0x20
-
+#define KEY_SPACEBAR     0x20
+#define KEY_BACKSPACE    0x08
 /*
   Mapping for converting incoming ALT letters to a standard case
 */
@@ -49,22 +50,28 @@
 
 
 // Screen specific player/bet coordinates
-const unsigned char playerXMaster[] = { 17,1, 1, 1, 15, 37,37, 37 };
-const unsigned char playerYMaster[] = { 19, 18, 11, 3, 2,3,11,18 };
+extern const unsigned char playerXMaster[];
+extern const unsigned char playerYMaster[];
+extern const char playerDirMaster[];
+extern const char playerBetXMaster[];
+extern const char playerBetYMaster[];
+extern const char playerCountIndex[];
 
-const char playerDirMaster[] = { 1,1,1,1,1,-1,-1,-1 };
-const char playerBetXMaster[] = { 1,10,10,10,3,-8,-8,-8 };
-const char playerBetYMaster[] = { -3, -2, 1,4,5,4,1,-2 };
+//const unsigned char playerXMaster[] = { 17,1, 1, 1, 15, 37,37, 37 };
+//const unsigned char playerYMaster[] = { 19, 18, 11, 3, 2,3,11,18 };
+
+//const char playerDirMaster[] = { 1,1,1,1,1,-1,-1,-1 };
+//const char playerBetXMaster[] = { 1,10,10,10,3,-8,-8,-8 };
+//const char playerBetYMaster[] = { -3, -2, 1,4,5,4,1,-2 };
 
 // Simple hard coded arrangment of players around the table based on player count.
 // These refer to index positions in the Master arrays above
 // Downside is new players will cause existing player positions to move.
 
 //                               2                3                4  
-const char playerCountIndex[] = {0,4,0,0,0,0,0,0, 0,2,6,0,0,0,0,0, 0,2,4,6,0,0,0,0, 
+//const char playerCountIndex[] = {0,4,0,0,0,0,0,0, 0,2,6,0,0,0,0,0, 0,2,4,6,0,0,0,0, 
 // 5                6                 7                8
-   0,2,3,5,6,0,0,0, 0,2,3,4,5,6,0,0,  0,2,3,4,5,6,7,0, 0,1,2,3,4,5,6,7};
-
+//   0,2,3,5,6,0,0,0, 0,2,3,4,5,6,0,0,  0,2,3,4,5,6,7,0, 0,1,2,3,4,5,6,7};
 
 
 #endif /* KEYMAP_H */
