ae9cbc8f357584cf34a3367504fc5f5389166b51
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2025-09-29 13:13:09 -0500

    [5cs][plus4] used ChatGPT to do a quick side-port. Needs lots of work.

---
+++ 5cardstud/cross-platform/src/plus4/appkey.c
@@ -0,0 +1,171 @@
+#ifdef __PLUS4__
+
+/**
+ * @brief   Cbm App Key Functions
+ * @author  Eric Carr
+ * @license gpl v. 3
+ */
+
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <cbm.h>
+#include "../platform-specific/appkey.h"
+#include "../misc.h"
+
+#define FUJICMD_OPEN_APPKEY 0xDC
+#define FUJICMD_WRITE_APPKEY 0xDE
+#define FUJICMD_READ_APPKEY 0xDD
+
+#define LFN 15
+
+typedef struct {
+  unsigned char command;
+  appkeyblock payload; // The appkey (see appkey.h)
+} appkeyC64;
+
+appkeyC64 appkey;
+
+#ifdef USE_EMULATOR
+  #define DEV 11
+  #define SEC_NUM 0
+  #define OPEN_APPKEY_PREFIX "appkey-"
+  #define OPEN_APPKEY_DELIM "-"
+#else
+  #define DEV 30  // The FujiNet device #
+  #define SEC_NUM 15
+  #define OPEN_APPKEY_PREFIX "openappkey:"
+  #define OPEN_APPKEY_DELIM ","
+#endif
+
+/**
+ * @brief Open a key for reading or writing
+ * @param open_mode 0 = read, 1 = write
+ * @param creator_id Key creator ID: ($0000-$FFFF)
+ * @param app_id The App ID ($00-$FF)
+ * @param key_id The Key ID ($00-$FF)
+ * @return error code
+ */
+unsigned char open_appkey(unsigned char open_mode, unsigned int creator_id, unsigned char app_id, char key_id)
+{ 
+  static unsigned char appkey_error;
+
+  #ifdef USE_EMULATOR  
+  
+  if (open_mode == APPKEY_WRITE)
+    strcpy(appkey.payload.write.value, "s:");
+  else
+    strcpy(appkey.payload.write.value, "");
+
+  itoa(creator_id, &appkey.payload.write.value[strlen(appkey.payload.write.value)], 16);
+
+  strcat(appkey.payload.write.value, OPEN_APPKEY_DELIM);
+  itoa(app_id, &appkey.payload.write.value[strlen(appkey.payload.write.value)], 16);
+  
+  strcat(appkey.payload.write.value, OPEN_APPKEY_DELIM);
+  itoa(key_id, &appkey.payload.write.value[strlen(appkey.payload.write.value)], 16);
+
+  strcat(appkey.payload.write.value, ".key");
+
+  // strcat(appkey.payload.write.value, OPEN_APPKEY_DELIM);
+  // itoa(open_mode, &appkey.payload.write.value[strlen(appkey.payload.write.value)], 10);
+
+  // strcat(appkey.payload.write.value, OPEN_APPKEY_DELIM);
+  // strcat(appkey.payload.write.value, "0");
+
+  if (open_mode == APPKEY_WRITE) {
+    appkey_error = cbm_open(LFN, DEV, 15, appkey.payload.write.value);
+    cbm_close(LFN);
+
+    appkey_error = cbm_open(LFN,DEV,open_mode,&appkey.payload.write.value[2]);
+  } else {
+    appkey_error = cbm_open(LFN,DEV,open_mode,&appkey.payload.write);
+  }
+  #else
+    appkey.command = FUJICMD_OPEN_APPKEY;
+    appkey.payload.open.creator = creator_id;
+    appkey.payload.open.app = app_id;
+    appkey.payload.open.key = key_id;
+    appkey.payload.open.mode = open_mode;
+    appkey.payload.open.reserved = 0;
+    cbm_open(LFN,DEV,SEC_NUM,"");
+    cbm_write(LFN, &appkey, sizeof(appkey));
+  #endif
+  
+  #ifdef USE_EMULATOR
+    return appkey_error;
+  #endif
+
+  cbm_close(LFN);
+  
+  return appkey_error;
+}
+
+
+
+void read_appkey(unsigned int creator_id, unsigned char app_id, unsigned char key_id, char* data)
+{
+  static unsigned char appkey_error;
+  static int read;
+  static unsigned int command = FUJICMD_READ_APPKEY;
+
+  // Set output as empty string
+  data[0]=0;
+
+  if (appkey_error = open_appkey(APPKEY_READ, creator_id, app_id, key_id))
+    return;
+  
+  #ifndef USE_EMULATOR
+    cbm_close(LFN);
+    appkey_error = cbm_open(LFN,DEV,SEC_NUM,(const char *)&command);
+  #endif
+
+  if (!appkey_error) {
+    read = cbm_read(LFN, &appkey.payload, sizeof(appkey.payload.read));
+
+    if (read>1 && appkey.payload.read.length <= MAX_APPKEY_LEN) {  
+        memcpy(data, appkey.payload.read.value, appkey.payload.read.length);
+        data[appkey.payload.read.length] = 0;
+    } else {
+        appkey_error = 64;
+    }
+
+    // Future: may need to call close_appkey
+  }
+
+  cbm_close(LFN);
+  return;
+}
+
+void write_appkey(unsigned int creator_id, unsigned char app_id, unsigned char key_id, char *data)
+{
+  static unsigned char appkey_error;
+  static unsigned char len;
+
+  if (appkey_error = open_appkey(APPKEY_WRITE, creator_id, app_id, key_id))
+    return;
+
+  len = strlen(data);
+  if (len > MAX_APPKEY_LEN)
+    len = MAX_APPKEY_LEN;
+
+  memset(&appkey.payload, 0, sizeof(appkey.payload.read));
+  appkey.payload.read.length = len;
+  memcpy(&appkey.payload.read.value, data, appkey.payload.read.length);
+
+  #ifdef USE_EMULATOR
+    cbm_write(LFN, &appkey.payload.read, sizeof(appkey.payload.read));
+  #else
+    appkey.command = FUJICMD_WRITE_APPKEY;
+    cbm_open(LFN,DEV,SEC_NUM,"");
+    cbm_write(LFN, &appkey, sizeof(appkey));
+  #endif
+
+  cbm_close(LFN);
+
+  // Future: may need to call close_appkey
+
+  return;
+}
+
+#endif /* __PLUS4__ */
---
+++ 5cardstud/cross-platform/src/plus4/network.c
@@ -0,0 +1,84 @@
+#ifdef __PLUS4__
+
+/*
+  Network functionality
+*/
+#include <plus4.h>
+#include <cbm.h>
+#include <string.h>
+#include <conio.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include "../platform-specific/appkey.h"
+#include "../fujinet-fuji.h"
+#include "../fujinet-network.h"
+#include "../misc.h"
+
+#define LFN 2     // The logical file number to use for I/O
+#define DEV 16    // The network device #
+#define SAN 2     // The secondary address (SA) to use on DEV.
+#define CMD 15    // The secondary address for the Command channel
+
+
+#ifndef USE_EMULATOR
+int getResponse(char *url, unsigned char *buffer, uint16_t max_len)
+{
+  static int16_t count;
+  /* network_open(url, OPEN_MODE_HTTP_GET_H, OPEN_TRANS_NONE); */
+  /* count = network_read(url, buffer, max_len); */
+  /* network_close(url); */
+
+  cbm_open(LFN,DEV,SAN,url);
+  count = cbm_read(LFN,buffer,max_len);
+  cbm_close(LFN);
+  
+  return count>0;
+}
+#else
+
+int getResponse(char *url, unsigned char *buffer, uint16_t max_len)
+{
+  int16_t count;
+  int8_t  i, wait;
+
+  // Delete existing file
+  cbm_open(15,11,15, "s:vice-in"); 
+
+  // Write command file (currently just the url )
+  cbm_open(LFN,11,1,"vice-out"); 
+  cbm_write(LFN, url, strlen(url)); 
+  cbm_close(LFN);
+
+  // Wait until command file no longer exists (signifies a response is ready) */
+  buffer[0]=0; 
+  wait=0; 
+  count = 1;
+  while (count != 0 ) {
+
+    // If we timed out (>15 seconds), return false 
+    wait++; 
+    if (wait > 90) {
+      return 0;
+    }
+
+    // Short delay
+    for (i=0;i<10;i++) { 
+      waitvsync(); 
+    } 
+
+    // Check if file exists by renaming it
+    count = cbm_open(15,11,15, "r0:vice-out=vice-out"); 
+    cbm_close(15); 
+  } 
+
+  // Read the response into the buffer 
+  cbm_open(LFN,11,0,"vice-in"); 
+  count = cbm_read(LFN,buffer, max_len); 
+  cbm_close(LFN); 
+
+  return count>0;
+}
+#endif
+
+
+#endif /* __PLUS4__ */
