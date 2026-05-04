d12e34f2d07eaaa3a81d3bed863a3a978888916d
Chris Osborn <fozztexx@fozztexx.com>
2026-05-04 10:32:12 -0700

    Fujitzee moved to https://github.com/FujiNetWIFI/fujinet-fujitzee

--- fujitzee/cross-platform/src/atari/charset.s
+++
@@ -1,6 +0,0 @@
-; Include the custom charset file, generated with Atari Fontmapper
-; Download: https://github.com/matosimi/atari-fontmaker
-.export _charset
-
-_charset:
-.incbin "support/atari/charset.fnt"
--- fujitzee/cross-platform/src/coco/input.c
+++
@@ -1,9 +0,0 @@
-#ifdef _CMOC_VERSION_
-
-//#include <peekpoke.h>
-
-unsigned char readJoystick() {
-  return 0; // todo - give real value
-}
-
-#endif
