390b92a8d177e5d610e316906b0c7de7c1073c73
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2026-06-05 19:46:39 -0500

    fix sprite-tool to use vdp calls.

--- sprite-tool/adam/src/cursor.c
+++ sprite-tool/adam/src/cursor.c
@@ -13,15 +13,15 @@ static const unsigned char selector_pattern_left[8] =
 
 void cursor(bool t)
 {
-  msx_vfill(0x3806,t == true ? 0xFF : 0x00, 2);
-  msx_vfill(0x1b00,0x00,4);
-  msx_vpoke(0x1b03,4);
+  vdp_vfill(0x3806,t == true ? 0xFF : 0x00, 2);
+  vdp_vfill(0x1b00,0x00,4);
+  vdp_vpoke(0x1b03,4);
 }
 
 void cursor_pos(unsigned char x, unsigned char y)
 {
-  msx_vpoke(0x1b00,(y<<3));
-  msx_vpoke(0x1b01,(x<<3));
+  vdp_vpoke(0x1b00,(y<<3));
+  vdp_vpoke(0x1b01,(x<<3));
 }
 
 void selector(unsigned char t)
@@ -29,24 +29,24 @@ void selector(unsigned char t)
   switch(t)
     {
     case 0:
-      msx_vfill(0x3800,0x00,8);
+      vdp_vfill(0x3800,0x00,8);
       break;
     case 1:
-      msx_vwrite(selector_pattern_right,0x3800,sizeof(selector_pattern_right));
+      vdp_vwrite(selector_pattern_right,0x3800,sizeof(selector_pattern_right));
       break;
     case 2:
-      msx_vwrite(selector_pattern_left,0x3800,sizeof(selector_pattern_left));
+      vdp_vwrite(selector_pattern_left,0x3800,sizeof(selector_pattern_left));
       break;
     }
 
   // Set color, clear position
   
-  msx_vfill(0x1b00,0x00,4);
-  msx_vpoke(0x1b03,8);
+  vdp_vfill(0x1b00,0x00,4);
+  vdp_vpoke(0x1b03,8);
 }
 
 void selector_pos(unsigned char x, unsigned char y)
 {
-  msx_vpoke(0x1b00,(y<<3)-1);
-  msx_vpoke(0x1b01,(x<<3));
+  vdp_vpoke(0x1b00,(y<<3)-1);
+  vdp_vpoke(0x1b01,(x<<3));
 }
